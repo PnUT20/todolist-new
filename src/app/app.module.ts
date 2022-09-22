cb9b609ebed41ea10cfe5e205d2971b27247a95b
Monirul <monirul.work.1729@gmail.com>
2022-09-22 16:02:13 +0530

    Made joke generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { JokeComponent } from './joke/joke.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    TodoListComponent,
+    TodoAddComponent,
+    JokeComponent,
+  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
